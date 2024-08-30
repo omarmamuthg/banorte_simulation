3a0d9781a17a401e511af386a708dcbe316a2507
omarmamuthg <omar.mamuthg@gmail.com>
2024-08-29 18:34:48 -0600

    se hace los inicios de codigo

---
+++ simulacion_banorte.py
@@ -0,0 +1,17 @@
+# Importamos modulos que se usaran a lo largo del programa.
+import datetime
+import re
+import random
+
+# Se crean las variables globales que se estarán trabajando.
+saldo = 10000
+
+# Creamos listas vacías donde guardaremos la referencia del movimiento, y en otra lista, los datos de la persona que hizo el movimiento.
+folio_deposito = []
+folio_retiro = []
+folio_transferencia = []
+datos_d= []
+datos_r= []
+datos_t= []
+
+print()
